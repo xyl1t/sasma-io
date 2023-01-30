ba1d97acd45f02262eda27a05d4126180640b82f
arndjus <arndjus@gitlab.com>
2023-01-30 14:45:06 +0100

    changed position of statements

--- common/systems/RenderingSystem.js
+++ common/systems/RenderingSystem.js
@@ -7,15 +7,15 @@ const query = defineQuery([Position, Sprite]);
 const meQuery = defineQuery([Me])
 
 export const renderingSystem = defineSystem((world) => {
-  const player = meQuery(world);
   const { canvas, ctx, images } = world;
   ctx.save();
   // ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.fillStyle = "lightgray";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
+
   ctx.translate(world.windowWidth / 2, world.windowHeight / 2);
-  
-  const renderAbleEntities = query(world);
+
+  const player = meQuery(world);
   for (const id of player) {
     ctx.drawImage(
       images[Sprite.texture[id]],
@@ -25,7 +25,7 @@ export const renderingSystem = defineSystem((world) => {
     ctx.translate(-Position.x[id], -Position.y[id]);
   }
   
-
+  const renderAbleEntities = query(world);
   for (const id of renderAbleEntities) {
     if (player[0] != id){
       ctx.drawImage(
@@ -34,8 +34,8 @@ export const renderingSystem = defineSystem((world) => {
         Position.y[id]
       );
     }
-    
   }
+  
   ctx.strokeStyle = "black";
   ctx.strokeRect(0, 0, 400, 300);
   ctx.restore();
