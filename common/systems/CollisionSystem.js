6f0336a4641dcd581ad94ceffb96f03ba27d43cd
Xylit <56483075+xyl1t@users.noreply.github.com>
2023-01-31 11:11:54 +0100

    rename and format

--- common/systems/CollisionSystem.js
+++ common/systems/CollisionSystem.js
@@ -1,25 +1,20 @@
-import {
-  defineQuery,
-  defineSystem,
-} from "../bitecs.js";
+import { defineQuery, defineSystem } from "../bitecs.js";
 
 import { Position } from "../components/Position.js";
 import { Player } from "../components/Player.js";
 
-// QUERIES // Selects all entities that have the following components
-const query = defineQuery([Position]);
-const collisionQuery = defineQuery([Position, Player]);
+const playerQuery = defineQuery([Position, Player]);
 
-// SYSTEMS // a function that runs through some entities and modifies their components
 export const collisionSystem = defineSystem((world) => {
-  const ents = collisionQuery(world);
+  const ents = playerQuery(world);
   for (const id of ents) {
-    let posX = Position.x[id]
-    let posY = Position.y[id]
-    let hypotenuse = Math.sqrt(Math.pow(posX, 2) + Math.pow(posY, 2))
+    let posX = Position.x[id];
+    let posY = Position.y[id];
+    // hypotenuse = distance from center of circle to player
+    let hypotenuse = Math.sqrt(Math.pow(posX, 2) + Math.pow(posY, 2));
     if (hypotenuse >= 400 || hypotenuse <= -400) {
-      Position.x[id] = posX * 400 / hypotenuse;
-      Position.y[id] = posY * 400 / hypotenuse;
+      Position.x[id] = (posX * 400) / hypotenuse;
+      Position.y[id] = (posY * 400) / hypotenuse;
     }
   }
   return world;
