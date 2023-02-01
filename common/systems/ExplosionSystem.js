24ed282218bba13efc38a12799e86ef91ddb45f4
Xylit <56483075+xyl1t@users.noreply.github.com>
2023-02-01 10:57:03 +0100

    refactor

--- common/systems/ExplosionSystem.js
+++ common/systems/ExplosionSystem.js
@@ -1,12 +1,12 @@
 import {
-    addComponent,
-    addEntity,
-    defineQuery,
-    defineSystem,
-    hasComponent,
-    Not,
-    removeEntity,
-  } from "../bitecs.js";
+  addComponent,
+  addEntity,
+  defineQuery,
+  defineSystem,
+  hasComponent,
+  Not,
+  removeEntity,
+} from "../bitecs.js";
 import { Body } from "../components/Body.js";
 import { Rotation } from "../components/Rotation.js";
 import { Position } from "../components/Position.js";
@@ -17,20 +17,20 @@ const bulletQuery = defineQuery([Not(Body), Position, Velocity]);
 const explosionQuery = defineQuery([Not(Body), Position, Not(Velocity)]);
 
 export const explosionSystem = defineSystem((world) => {
-    
-    const bulletEntities = bulletQuery(world);
-    for (const id of bulletEntities) {
-        let length = Math.sqrt(Math.pow(Velocity.x[id], 2) + Math.pow(Velocity.y[id], 2));
-        if (length < 200){
-            removeEntity(world, id);
-            const explosionId = addEntity(world);
-            addComponent(world, Position, explosionId);
-            Position.x[explosionId] = Position.x[id];
-            Position.y[explosionId] = Position.y[id];
+  const bulletEntities = bulletQuery(world);
+  for (const id of bulletEntities) {
 
-            addComponent(world, Sprite, explosionId);
-            Sprite.texture[explosionId] = world.assetIdMap.oilSpill_large;
-        } 
+    const length = Math.sqrt(Velocity.x[id] ** 2 + Velocity.y[id] ** 2);
+
+    if (length < 200) { // TODO: export to some constant or to bullet/gun comoponent
+      removeEntity(world, id);
+      const explosionId = addEntity(world);
+      addComponent(world, Position, explosionId);
+      Position.x[explosionId] = Position.x[id];
+      Position.y[explosionId] = Position.y[id];
+
+      addComponent(world, Sprite, explosionId);
+      Sprite.texture[explosionId] = world.assetIdMap.oilSpill_large;
     }
+  }
 });
-      
